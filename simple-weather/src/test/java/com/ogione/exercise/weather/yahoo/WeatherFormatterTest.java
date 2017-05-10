c0f11b68be95b04ea908ce38c7128a1256ae7b04
Yasuyuki Ogi <yasuyuki.ogi@gmail.com>
2017-05-10 16:47:57 +1000

    Initial commit from Eclipse

---
+++ simple-weather/src/test/java/com/ogione/exercise/weather/yahoo/WeatherFormatterTest.java
@@ -0,0 +1,28 @@
+package com.ogione.exercise.weather.yahoo;
+
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+
+import com.ogione.exercise.weather.Weather;
+import com.ogione.exercise.weather.WeatherFormatter;
+import com.ogione.exercise.weather.YahooParser;
+
+import junit.framework.TestCase;
+
+public class WeatherFormatterTest extends TestCase {
+
+	public WeatherFormatterTest(String name) {
+		super(name);
+	}
+	
+	public void testFormat() throws Exception {
+		InputStream nyData = 
+			getClass().getClassLoader().getResourceAsStream("ny-weather.xml");
+		Weather weather = new YahooParser().parse( nyData );
+		String formattedResult = new WeatherFormatter().format( weather );
+		InputStream expected = 
+			getClass().getClassLoader().getResourceAsStream("format-expected.dat");
+		assertEquals( IOUtils.toString( expected ), formattedResult );
+	}
+}
